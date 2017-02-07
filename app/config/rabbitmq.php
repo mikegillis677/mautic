a783452ccc7a6f1429ae6f884e5b788a700f26e4
sabativi <victor.sabatier@gmail.com>
2017-02-07 15:40:06 +0100

    init queue bundle

--- app/config/rabbitmq.php
+++ app/config/rabbitmq.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * @package     Mautic
- * @copyright   2014 Mautic Contributors. All rights reserved.
+ * @copyright   2014 Mautic Contributors. All rights reserved
  * @author      Mautic
+ *
  * @link        http://mautic.org
- * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @license     GNU/GPLv3 http://www.gnu.org/licenses/gpl3.0.html
  */
-
 $container->loadFromExtension(
     'old_sound_rabbit_mq',
     array(
@@ -19,31 +19,31 @@ $container->loadFromExtension(
                 'vhost' => '/',
                 'lazy' => true,
                 'connection_timeout' => 3,
-                'heartbeat' => 2,
+                'heartbeat'          => 2,
                 'read_write_timeout' => 4,
-            )
-        ),
-        'producers'       => array(
-            'task_email' => array(
-                'connection'  => 'default',
-                'exchange_options' => array(
+            ],
+        ],
+        'producers' => [
+            'task_email' => [
+                'connection'       => 'default',
+                'exchange_options' => [
                     'name' => 'task_email',
-                    'type' => 'direct'
-                )
-            )
-        ),
-        'consumers' => array(
-            'task_email' => array(
-              'connection' => 'default',
-              'exchange_options' => array(
+                    'type' => 'direct',
+                ],
+            ],
+        ],
+        'consumers' => [
+            'task_email' => [
+              'connection'       => 'default',
+              'exchange_options' => [
                   'name' => 'task_email',
-                  'type' => 'direct'
-              ),
-              'queue_options' => array(
-                'name' => 'task_email'
-              ),
-              'callback' => 'email_consumer'
-            )
-        )
-    )
+                  'type' => 'direct',
+              ],
+              'queue_options' => [
+                'name' => 'task_email',
+              ],
+              'callback' => 'email_consumer',
+            ],
+        ],
+    ]
 );
